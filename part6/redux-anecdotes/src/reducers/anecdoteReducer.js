cf82ce0b437369775c0394ab6c1ca68e11bf5d89
iobtl <tedmundhtl@gmail.com>
2020-05-31 13:28:37 +0800

    db.json integration

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,11 +1,4 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-];
+import anecdoteService from '../services/anecdotes';
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
@@ -16,8 +9,6 @@ const asObject = (anecdote) => {
     votes: 0,
   };
 };
-
-const initialState = anecdotesAtStart.map(asObject);
 
 // Action creators
 export const voteAnecdote = (id) => {
@@ -38,7 +29,14 @@ export const createNewAnecdote = (content) => {
   };
 };
 
-const anecdoteReducer = (state = initialState, action) => {
+export const initializeAnecdotes = (data) => {
+  return {
+    type: 'INIT_ANECDOTE',
+    data: data,
+  };
+};
+
+const anecdoteReducer = (state = [], action) => {
   console.log('state now: ', state);
   console.log('action', action);
   switch (action.type) {
@@ -62,7 +60,12 @@ const anecdoteReducer = (state = initialState, action) => {
         votes: 0,
       };
 
+      anecdoteService.create(newAnecdote);
+
       return [...state, newAnecdote];
+
+    case 'INIT_ANECDOTE':
+      return action.data;
 
     default:
       return state;
