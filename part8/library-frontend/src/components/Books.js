a408bb0075ddd8343d7a91aca1b1a5c6350639ff
iobtl <tedmundhtl@gmail.com>
2020-06-07 09:04:53 +0800

    Filter by genres

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Books = ({ books, show }) => {
+  const [genreFilter, setGenreFilter] = useState(null);
   if (!show) {
     return null;
   }
 
+  const bookGenres = books.map((book) => book.genres).flat();
+  const uniqueGenres = [...new Set(bookGenres)];
+
+  // Applying filter logic
+  const filteredBooks = genreFilter
+    ? books.filter((book) => book.genres.includes(genreFilter))
+    : books;
+
   return (
     <div>
       <h2>books</h2>
-
+      {genreFilter === null ? null : (
+        <div>
+          in genre <strong>{genreFilter}</strong>
+        </div>
+      )}
       <table>
         <tbody>
           <tr>
@@ -16,7 +29,7 @@ const Books = ({ books, show }) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -25,6 +38,12 @@ const Books = ({ books, show }) => {
           ))}
         </tbody>
       </table>
+      <div>
+        {uniqueGenres.map((genre) => (
+          <button onClick={() => setGenreFilter(genre)}>{genre}</button>
+        ))}
+        <button onClick={() => setGenreFilter(null)}>all genres</button>
+      </div>
     </div>
   );
 };
